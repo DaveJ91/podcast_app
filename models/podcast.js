47203cc272ce5f59656f62fe4375fe6b9c8edfe4
unknown <david.omeara91@gmail.com>
2020-01-29 15:09:31 +0100

    added model files and completed podcast model

---
+++ models/podcast.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PodcastSchema = new Schema(
+    {
+        name: {type: String, required: true, max: 100},
+        host: {type: Schema.Types.ObjectId, ref: 'Host', required: true},
+        started: {type: Date},
+        youtube: {type: String},
+        soundcloud: {type: String},
+        genre: {type: Schema.Types.ObjectId, ref: 'Genre', required: true}
+
+    }
+)
+
+
+// Virtual for the Podcast URL
+PodcastSchema
+    .virtual('url')
+    .get('url')
+    .get(function(){
+        return '/podcast' + this._id
+    });
+
+// Export Model
+module.exports = mongoose.model('Podcast', PodcastSchema)
+
