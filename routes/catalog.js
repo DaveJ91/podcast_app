738efd894aea343d0a0b5e5c014787abc61abc11
unknown <david.omeara91@gmail.com>
2020-01-30 10:51:35 +0100

    finished routes

--- routes/catalog.js
+++ routes/catalog.js
@@ -3,13 +3,31 @@ var router = express.Router();
 
 // Require Controllers - NOT IMPLEMENTED YET
 
-
-// PODCAST ROUTES
+/////////////////////////////////////////////
+/////// PODCAST ROUTES //////////////////////
+/////////////////////////////////////////////
 
 // PODCAST - List View
 router.get('/podcasts', podcast_controller.podcast_list);
 
-// SHOW ROUTES
+// PODCAST - Detail View
+router.get('/podcasts/:id', podcast_controller.podcast_detail);
+
+// PODCAST - Create View
+router.get('/podcasts/create', podcast_controller.podcast_create_get);
+router.post('/podcasts/create', podcast_controller.podcast_create_post);
+
+// PODCAST - Update View
+router.get('/podcasts/:id/update', podcast_controller.podcast_update_get);
+router.post('/podcasts/:id/update', podcast_controller.podcast_update_post);
+
+// PODCAST - Delete View
+router.get('/podcasts/:id/delete', podcast_controller.podcast_delete_get);
+router.post('/podcasts/:id/delete', podcast_controller.podcast_delete_post);
+
+////////////////////////////////////////////
+//// SHOW ROUTES //////////////////////////
+///////////////////////////////////////////
 
 // SHOW - List View
 router.get('/shows', show_controller.show_list);
@@ -30,14 +48,71 @@ router.get('/shows/:id/delete', show_controller.show_delete_get);
 router.post('/shows/:id/delete', show_controller.show_delete_post);
 
 
+//////////////////////////////////////////
+///////// PERSON ROUTES  /////////////////
+//////////////////////////////////////////
 
-// PERSON ROUTES
+// PEOPLE - List View
 router.get('/people', person_controller.person_list);
 
-// TOPIC ROUTES
-router.get('/topics', topic_controller.topic_list);
+// PEOPLE - Detail View
+router.get('/people/:id', person_controller.person_detail);
 
-// GENRE ROUTES
-router.get('/genres', genre_controller.genre_list);
+// PEOPLE - Create View
+router.get('/people/create', person_controller.person_create_get);
+router.post('/people/create', person_controller.person_create_post);
+
+// PEOPLE - Update View
+router.get('/people/:id/update', person_controller.person_update_get);
+router.post('/people/:id/update', person_controller.person_update_post);
+
+// PEOPLE - Delete View
+router.get('/people/:id/delete', person_controller.person_delete_get);
+router.post('/people/:id/delete', person_controller.person_delete_post);
+
+////////////////////////////
+///////// TOPIC ROUTES /////
+////////////////////////////
+
+// TOPIC - List View
+router.get('/people', topic_controller.topic_list);
+
+// TOPIC - Detail View
+router.get('/people/:id', topic_controller.topic_detail);
+
+// TOPIC - Create View
+router.get('/people/create', topic_controller.topic_create_get);
+router.post('/people/create', topic_controller.topic_create_post);
+
+// TOPIC - Update View
+router.get('/people/:id/update', topic_controller.topic_update_get);
+router.post('/people/:id/update', topic_controller.topic_update_post);
+
+// TOPIC - Delete View
+router.get('/people/:id/delete', topic_controller.topic_delete_get);
+router.post('/people/:id/delete', topic_controller.topic_delete_post);
+
+//////////////////////////////
+/////// GENRE ROUTES /////////
+//////////////////////////////
+
+// GENRE - List View
+router.get('/people', genre_controller.genre_list);
+
+// GENRE - Detail View
+router.get('/people/:id', genre_controller.genre_detail);
+
+// GENRE - Create View
+router.get('/people/create', genre_controller.genre_create_get);
+router.post('/people/create', genre_controller.genre_create_post);
+
+// GENRE - Update View
+router.get('/people/:id/update', genre_controller.genre_update_get);
+router.post('/people/:id/update', genre_controller.genre_update_post);
+
+// GENRE - Delete View
+router.get('/people/:id/delete', genre_controller.genre_delete_get);
+router.post('/people/:id/delete', genre_controller.genre_delete_post);
+
 
 module.exports = router;
