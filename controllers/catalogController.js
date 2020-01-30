38736e2ab14c34bf305c1e465d4de8a4e29d1b94
unknown <david.omeara91@gmail.com>
2020-01-30 12:54:03 +0100

    added templates

---
+++ controllers/catalogController.js
@@ -0,0 +1,25 @@
+var Podcast = require('../models/podcast')
+var Show = require('../models/show')
+var Person = require('../models/person')
+var Topic = require('../models/topic')
+var Genre = require('../models/genre')
+
+// TO DO: Add validators and sanitizers - for the Forms
+
+var async = require('async');
+
+// Home Page
+exports.home = (req, res) {
+
+    async.parallel({
+        Podcast_count: function(callback){
+            Podcast.count(callback);
+        },
+        Show_count: function(callback){
+            Show.count(callback);
+        },
+    }, function(err, results){
+        res.render('home', {title: 'Podcast App Home', error:err, data:results });
+    }
+    })
+}
