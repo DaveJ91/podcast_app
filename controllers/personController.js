b65fb788d2434a0bfe566a982af1acaac7beb19f
unknown <david.omeara91@gmail.com>
2020-01-31 12:24:14 +0100

    more progress on create and list views

--- controllers/personController.js
+++ controllers/personController.js
@@ -12,11 +12,11 @@ var async = require('async');
 exports.person_list = function(req, res, next){
 
     Person.find()
-        .sort([['name', 'ascending']])
+        .sort([['last_name', 'ascending']])
         .exec(function(err, list_persons) {
             if (err) {return next(err)}
             else {
-                res.render('people_list', { title: 'People List', person_list: list_persons});
+                res.render('person_list', { title: 'People List', person_list: list_persons});
             }
         });
 };
