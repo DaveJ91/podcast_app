f38cd431bdcd705c95fc4782c04cff6378c4c1e3
unknown <david.omeara91@gmail.com>
2020-01-31 11:58:31 +0100

    progress on create and list views

--- controllers/podcastController.js
+++ controllers/podcastController.js
@@ -4,6 +4,9 @@ var Person = require('../models/person')
 var Topic = require('../models/topic')
 var Genre = require('../models/genre')
 
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 // TO DO: Add validators and sanitizers
 
 var async = require('async');
@@ -40,7 +43,72 @@ exports.podcast_create_get = function(req, res, next){
 };
 
 // Create View - POST
-exports.podcast_create_post = function(req, res, next){
-    console.log(req)
-}
+exports.podcast_create_post = [
+
+
+    // Convert the Genre to an Array
+    (req, res, next) => {
+        if(!(req.body.genre instanceof Array)){
+            if(typeof req.body.genre==='undefined')
+            req.body.genre=[];
+            else
+            req.body.genre=new Array(req.body.genre);
+        }
+        next();
+    },
+
+    //Validate the Fields
+    body('name', 'Name must not be empty.').isLength({min:1}).trim(),
+    // body('host', 'Host must not be empty.').isLength({min:1}).trim(),
+    
+
+    //Sanitize the fields
+    sanitizeBody('*').escape(),
+    
+    // After validation and sanitization process request
+    (req, res, next) => {
+
+        // Extract the validation errors from the request
+        const errors = validationResult(req);
+
+        // Create a Podcast Object with escaped and trimmed data
+        var podcast = new Podcast(
+            {
+                name: req.body.name,
+                // host: req.body.host,
+                // genre: req.body.genre,
+                youtube_link: req.body.youtube,
+                soundcloud_link: req.body.soundcloud
+
+             }
+        );
+
+        if (!errors.isEmpty()) {
+            console.log(errors)
+            // if there's errors rerender the form
+
+            async.parallel({
+                hosts: function(callback){
+                    Person.find(callback);
+                },
+                genres: function(callback){
+                    Genre.find(callback)
+                },
+            }, function(err, results) {
+                if (err) {return next(err); }
+                
+            });
+            return;
+        }
+
+        else {
+            // If the form data is valid, save the Podcast
+            podcast.save(function(err) {
+                console.log("save podcast")
+                if(err) {return next(err); }
+                res.redirect('/catalog')
+            })
+        }
+    }
+]
 
